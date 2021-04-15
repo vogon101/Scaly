8bffe4177cff08a2cd86e01fcf545187b9c3dee5
Freddie Poser <freddie.poser@gmail.com>
2021-04-15 16:52:58 +0100

    Add tests for tuple matching

--- test_files/compileTest.scala
+++ test_files/compileTest.scala
@@ -2,50 +2,30 @@ object Main {
 
   def main() : Unit = {
 
+    print(Vector(1,2))
 
-    val x = "Hello" match {
-      case "Goodbye" => 100
-      case "Hello" => 200
-      case "Nothing" => 300
-    }
-    print("X: " + str(x))
-
-    val y = x match {
-      case 20 => "Not this"
-      case 30 => "Not this"
-      case 200 => "this"
-    }
-
-    print("Y: " + y)
-
-    val z = y match {
-      case "Not this" => "Not this either"
-      case p => "This: " + p
-    }
-
-    print("Z: " + z)
-
-    val q = "abc" match { case x => ("def" match { case x => "One "}) + x }
-    print(q)
-
-    val t = (1, (2, 3))
-
-    val p = t match {
-      case (x, (y,z)) => str(x) + str(y) + str(z)
-    }
-
-    val q = t match {
-      case (0, x) => "Pattern zero " + str(x)
-      case (1, (20, x)) => "Pattern One, Twenty " + str(x)
-      case (1, (2, x)) => "Pattern One, Two  " + str(x)
-    }
-
-    print(p)
-
-    print(q)
-
+    val v = new Vector(1,2)
+    print(bar(v.foo))
+    val z = bar
+    print(z((x : Int) => x + 2))
     ()
 
   }
+
+  def bar(f: Int => Int): Int = f(10)
+
+
+}
+
+
+
+class Vector (val x: Int, val y: Int) {
+
+  def foo(z: Int): Int = z + x
+
+}
+object Vector {
+
+  def apply(x: Int, y: Int): Vector = new Vector(x,y)
 
 }
